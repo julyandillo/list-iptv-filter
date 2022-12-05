5fc552b6d71f577f55c9c754c65c8dad8c4b2262
sobremesa <julyandillo@gmail.com>
2022-12-05 21:45:24 +0100

    Mostrando un menú con los grupos encontrados para poder seleccionar cual se quiere extraer

--- main.py
+++ main.py
@@ -10,8 +10,45 @@ def main():
 
     my_m3u8 = MyM3u8(downloader.download_file_if_not_exists())
 
-    with open('lists/iptv_MUNDIAL.m3u', 'w') as file:
-        file.write(my_m3u8.extract_group_from_list('07. MUNDIAL QATAR 2022'))
+    available_groups = sorted(my_m3u8.get_groups())
+
+    selected_group = available_groups[show_menu_with_groups_availables(available_groups)]
+
+    group_name = sanitize_group_name(selected_group.split('.')[1])
+
+    save_filter_list(get_output_filename_for_group_name(group_name), my_m3u8.extract_group_from_list(selected_group))
+
+
+def show_menu_with_groups_availables(groups: list) -> int:
+    print("Grupos disponibles:")
+    print("------------------------------------")
+    for group in groups:
+        print(group)
+
+    group_selected = show_user_input()
+
+    while group_selected < 0 or group_selected > len(groups):
+        print("ERROR: no es un grupo válido")
+        group_selected = show_user_input()
+
+    return group_selected
+
+
+def show_user_input() -> int:
+    return int(input("Grupo para extraer: ")) - 1
+
+
+def sanitize_group_name(group: str) -> str:
+    return group.strip().replace(' ', '_')
+
+
+def get_output_filename_for_group_name(group_name: str) -> str:
+    return f"lists/{group_name}.m3u"
+
+
+def save_filter_list(filename: str, groups: str) -> None:
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(groups)
 
 
 if __name__ == '__main__':
